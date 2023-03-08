b8406d50a8c3c8aca3a5f282690fbd06a74df3e3
Jude <judecodes@gmail.com>
2023-03-08 09:29:07 +0100

    debugging

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+console.log(process.env.NEXT_PUBLIC_BASE_URL);
 
 const http = axios.create({
-  baseURL
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL
 });
 
 export default http;
