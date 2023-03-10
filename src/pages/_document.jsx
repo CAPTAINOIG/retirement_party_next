67389273844ba3763a9c2d321eb5bd14ca0ea342
Jude <judecodes@gmail.com>
2023-03-10 01:19:44 +0100

    remove Lexend font

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -15,12 +15,6 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet"/>
-
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"/>
         </Head>
         <body>
         <Main/>
