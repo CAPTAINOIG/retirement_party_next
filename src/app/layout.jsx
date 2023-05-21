ff233268511bb674972cc3e4d5b093fa5cab0e26
Jude <judecodes@gmail.com>
2023-05-21 16:02:27 +0100

    fix(root-layout): useLayoutEffect error

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,23 +1,12 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { AuthProvider } from "@/hooks/use-auth";
 import { ToastProvider } from "@/hooks/use-toast";
 import QueryProvider from "@/components/QueryProvider";
-import { useIsomorphicLayoutEffect } from "react-use";
 import './global.scss';
 import 'swiper/css/bundle';
+import ResizeScreenHeight from "@/components/ResizeScreenHeight";
 
 const RootLayout = ({ children }) => {
-  const handleResize = useCallback(() => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${ vh }px`);
-  }, []);
-
-  useIsomorphicLayoutEffect(() => {
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
   return (
     <html lang="en">
     <head>
@@ -31,6 +20,7 @@ const RootLayout = ({ children }) => {
       <AuthProvider>
         <ToastProvider>
           { children }
+          <ResizeScreenHeight/>
         </ToastProvider>
       </AuthProvider>
     </QueryProvider>
