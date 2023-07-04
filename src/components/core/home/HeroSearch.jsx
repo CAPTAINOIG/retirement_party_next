69f3a760d8a319ebb79ca4704c37ed9f5f22da73
Jude <judecodes@gmail.com>
2023-07-04 13:49:53 +0100

    feat(hero-search): minor ui tweaks

--- src/components/core/home/HeroSearch.jsx
+++ src/components/core/home/HeroSearch.jsx
@@ -28,16 +28,16 @@ const HeroSearch = ({ className }) => {
 
   return (
     <div className={ classNames('relative max-w-2xl px-4 mx-auto sm:px-6', className) }>
-      <div className="relative w-full max-w-xl mx-auto rounded-full h-[70px] lg:max-w-none text-white">
+      <div className="relative w-full max-w-xl mx-auto rounded-full h-[56px] md:h-[66px] lg:max-w-none text-white">
         <input
           value={ query } onChange={ handleChange }
           onKeyUp={ e => e.key.toLowerCase() === 'enter' && handleSearch() }
           type="text" name="query" id="query" placeholder="Find infographics.."
-          className="rounded-full w-full h-[70px] py-0 pl-8 pr-16 outline-none bg-slate-200/10 shadow-md hover:outline-none focus:ring-primary-200 focus:border-primary-200"
+          className="rounded-full w-full h-[56px] md:h-[66px] py-0 pl-8 pr-16 outline-none bg-slate-200/10 shadow-md hover:outline-none focus:ring-primary-200 focus:border-primary-200"
         />
         <IconButton
           icon={ <IconSearch size="20"/> } rounded onClick={ handleSearch } disabled={ query.length < 2 }
-          color="white" type="submit" className="absolute inline-flex items-center right-4 top-4"
+          color="white" type="submit" className="absolute inline-flex items-center right-4 top-1/2 -translate-y-1/2"
         />
       </div>
     </div>
