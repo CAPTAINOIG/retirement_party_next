3a161c745594ec7d460cf31c6cf3266d8e4c0224
Jude <judecodes@gmail.com>
2023-07-07 09:51:35 +0100

    refactor dashboard categories and items representation
    This commit expands the items in the dashboard and organizes them into different categories. Previously, the items were a flat list without a categorization system and with limited information. This change adds much more detail to each item and groups them into a list of distinct categories for improved organization and easier navigation. Each item now includes a list of categories it belongs to. The dashboard page has been updated to render these items under their respective groups. Not only does this improve the user experience, but it also allows for future enhancements and expansions of the dashboard with additional items and categories.

--- src/app/dashboard/(root)/page.jsx
+++ src/app/dashboard/(root)/page.jsx
@@ -3,22 +3,30 @@ import React, { createElement } from 'react';
 import requireAuth from "@/guards/require-auth";
 import classNames from "classnames";
 import {
+  IconAssembly,
   IconBooks,
   IconBriefcase,
-  IconChartArcs3,
-  IconChartArrows,
-  IconChartCircles,
+  IconBuildingHospital,
+  IconChartDots3,
+  IconChartHistogram,
+  IconChartPie4,
+  IconClipboardText,
+  IconCreditCard,
+  IconEditCircle,
+  IconFileChart,
   IconFileText,
-  IconHeadphones,
+  IconGasStation,
+  IconGavel,
   IconMessageChatbot,
+  IconPaperBag,
   IconReportMoney,
-  IconUsers,
+  IconUserCircle,
   IconUsersGroup
 } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
 import Card from "@/components/global/Card";
 
-const products = [
+const items = [
   {
     name: 'Bank statement',
     slug: 'bank-statement',
@@ -28,18 +36,8 @@ const products = [
     icon: IconFileText,
     homeLink: '/statement',
     dashboardLink: '/dashboard/statement',
-    status: 'active'
-  },
-  {
-    name: 'Sensiii',
-    slug: 'sensii',
-    description: `An advanced embedded AI assistant`,
-    backgroundColor: 'bg-blue-500',
-    textColor: 'text-blue-500',
-    icon: IconMessageChatbot,
-    homeLink: '/sensii',
-    dashboardLink: '/dashboard/sensii',
-    status: 'active'
+    status: 'active',
+    categories: ['featured', 'general', 'financial']
   },
   {
     name: 'Financial report',
@@ -50,96 +48,223 @@ const products = [
     icon: IconBooks,
     homeLink: '/financial-report',
     dashboardLink: '/dashboard/financial-report',
-    status: 'active'
+    status: 'active',
+    categories: ['featured', 'investment']
   },
   {
-    name: 'Customer report',
-    slug: 'customer-report',
-    description: `Analyze customers reports in seconds`,
+    name: 'Chat assistant',
+    slug: 'sensii',
+    description: `An advanced embedded AI assistant`,
+    backgroundColor: 'bg-blue-500',
+    textColor: 'text-blue-500',
+    icon: IconMessageChatbot,
+    homeLink: '/sensii',
+    dashboardLink: '/dashboard/sensii',
+    status: 'active',
+    categories: ['general']
+  },
+  {
+    name: 'Custom report',
+    slug: 'custom-report',
+    description: `Analyze customs reports in seconds`,
     backgroundColor: 'bg-orange-500',
     textColor: 'text-orange-500',
-    icon: IconUsers,
-    homeLink: '/customer-report',
-    dashboardLink: '/dashboard/customer-reports',
-    status: 'coming-soon'
+    icon: IconEditCircle,
+    homeLink: '/custom-report',
+    dashboardLink: '/dashboard/custom-reports',
+    status: 'coming-soon',
+    categories: ['featured', 'general']
   },
   {
-    name: 'Staff hiring',
-    slug: 'staff-hiring',
-    description: `Make better hiring decisions`,
+    name: 'Invoices',
+    slug: 'invoices',
+    description: `Get insights on your invoices`,
+    backgroundColor: 'bg-indigo-500',
+    textColor: 'text-indigo-500',
+    icon: IconClipboardText,
+    homeLink: '/invoices',
+    dashboardLink: '/dashboard/invoices',
+    status: 'coming-soon',
+    categories: ['general']
+  },
+  {
+    name: 'Expenses',
+    slug: 'expenses',
+    description: `Get insights on your expenses`,
+    backgroundColor: 'bg-teal-500',
+    textColor: 'text-teal-500',
+    icon: IconReportMoney,
+    homeLink: '/expenses',
+    dashboardLink: '/dashboard/expenses',
+    status: 'coming-soon',
+    categories: ['general']
+  },
+  {
+    name: 'Customer profile',
+    slug: 'customer-profile',
+    description: `Understand your customers`,
+    backgroundColor: 'bg-yellow-500',
+    textColor: 'text-yellow-500',
+    icon: IconUserCircle,
+    homeLink: '/customer-profile',
+    dashboardLink: '/dashboard/customer-profile',
+    status: 'coming-soon',
+    categories: ['financial', 'insurance']
+  },
+  {
+    name: 'Credit Portfolio',
+    slug: 'credit-portfolio',
+    description: `Maintain your credit portfolio`,
     backgroundColor: 'bg-red-500',
     textColor: 'text-red-500',
-    icon: IconUsersGroup,
-    homeLink: '/staff-hiring',
-    dashboardLink: '/dashboard/staff-hiring',
-    status: 'coming-soon'
+    icon: IconCreditCard,
+    homeLink: '/credit-portfolio',
+    dashboardLink: '/dashboard/credit-portfolio',
+    status: 'coming-soon',
+    categories: ['financial']
   },
   {
-    name: 'Portfolio report',
-    slug: 'portfolio-report',
-    description: `Analyze your portfolio in just a few steps`,
+    name: 'Investment Portfolio',
+    slug: 'investment-portfolio',
+    description: `Track your investments`,
+    backgroundColor: 'bg-purple-500',
+    textColor: 'text-purple-500',
+    icon: IconBriefcase,
+    homeLink: '/investment-portfolio',
+    dashboardLink: '/dashboard/investment-portfolio',
+    status: 'coming-soon',
+    categories: ['financial']
+  },
+  {
+    name: 'Saving portfolio',
+    slug: 'saving-portfolio',
+    description: `Monitor your savings`,
+    backgroundColor: 'bg-indigo-500',
+    textColor: 'text-indigo-500',
+    icon: IconPaperBag,
+    homeLink: '/saving-portfolio',
+    dashboardLink: '/dashboard/saving-portfolio',
+    status: 'coming-soon',
+    categories: ['financial']
+  },
+  {
+    name: 'Credit Modelling',
+    slug: 'credit-modelling',
+    description: `Model your credit score`,
     backgroundColor: 'bg-orange-500',
     textColor: 'text-orange-500',
-    icon: IconBriefcase,
-    homeLink: '/mineme',
-    dashboardLink: '/dashboard/mineme',
-    status: 'coming-soon'
+    icon: IconChartHistogram,
+    homeLink: '/credit-modelling',
+    dashboardLink: '/dashboard/credit-modelling',
+    status: 'coming-soon',
+    categories: ['financial']
+  },
+  {
+    name: 'Trading insight',
+    slug: 'trading-insight',
+    description: `Get insights on your trades`,
+    backgroundColor: 'bg-blue-500',
+    textColor: 'text-blue-500',
+    icon: IconFileChart,
+    homeLink: '/trading-insight',
+    dashboardLink: '/dashboard/trading-insight',
+    status: 'coming-soon',
+    categories: ['investment']
+  },
+  {
+    name: 'Hospital distribution',
+    slug: 'hospital-distribution',
+    description: `Manage hospital distributions`,
+    backgroundColor: 'bg-purple-500',
+    textColor: 'text-purple-500',
+    icon: IconBuildingHospital,
+    homeLink: '/hospital-distribution',
+    dashboardLink: '/dashboard/hospital-distribution',
+    status: 'coming-soon',
+    categories: ['insurance']
+  },
+  {
+    name: 'Risk assessment',
+    slug: 'risk-assessment',
+    description: `Perform risk assessments`,
+    backgroundColor: 'bg-orange-500',
+    textColor: 'text-orange-500',
+    icon: IconAssembly,
+    homeLink: '/risk-assessment',
+    dashboardLink: '/dashboard/risk-assessment',
+    status: 'coming-soon',
+    categories: ['insurance']
+  },
+  {
+    name: 'Legal',
+    slug: 'legal',
+    description: `Manage your legal matters`,
+    backgroundColor: 'bg-yellow-500',
+    textColor: 'text-yellow-500',
+    icon: IconGavel,
+    homeLink: '/legal',
+    dashboardLink: '/dashboard/legal',
+    status: 'coming-soon',
+    categories: ['cooking']
+  },
+  {
+    name: 'Oil and gas',
+    slug: 'oil-and-gas',
+    description: `Manage your oil and gas operations`,
+    backgroundColor: 'bg-indigo-500',
+    textColor: 'text-indigo-500',
+    icon: IconGasStation,
+    homeLink: '/oil-and-gas',
+    dashboardLink: '/dashboard/oil-and-gas',
+    status: 'coming-soon',
+    categories: ['cooking']
+  },
+  {
+    name: 'Government',
+    slug: 'government',
+    description: `Interact with government operations`,
+    backgroundColor: 'bg-teal-500',
+    textColor: 'text-teal-500',
+    icon: IconUsersGroup,
+    homeLink: '/government',
+    dashboardLink: '/dashboard/government',
+    status: 'coming-soon',
+    categories: ['cooking']
+  },
+  {
+    name: 'Financial Model',
+    slug: 'financial-model',
+    description: `Create and manage financial models`,
+    backgroundColor: 'bg-blue-500',
+    textColor: 'text-blue-500',
+    icon: IconChartPie4,
+    homeLink: '/financial-model',
+    dashboardLink: '/dashboard/financial-model',
+    status: 'coming-soon',
+    categories: ['advisory']
   },
   {
     name: 'Investment analysis',
     slug: 'investment-analysis',
-    description: `Get investment analysis in just a few steps`,
-    backgroundColor: 'bg-cyan-500',
-    textColor: 'text-cyan-500',
-    icon: IconChartArcs3,
+    description: `Analyze your investments`,
+    backgroundColor: 'bg-orange-500',
+    textColor: 'text-orange-500',
+    icon: IconChartDots3,
     homeLink: '/investment-analysis',
     dashboardLink: '/dashboard/investment-analysis',
-    status: 'coming-soon'
-  },
-  {
-    name: 'Customer support',
-    slug: 'customer-support',
-    description: `Get insights on your customer support`,
-    backgroundColor: 'bg-rose-500',
-    textColor: 'text-rose-500',
-    icon: IconHeadphones,
-    homeLink: '/customer-support',
-    dashboardLink: '/dashboard/customer-support',
-    status: 'coming-soon'
-  },
-  {
-    name: 'Procurement analysis',
-    slug: 'procurement-analysis',
-    description: `Get your procurement analysis in a few steps`,
-    backgroundColor: 'bg-teal-500',
-    textColor: 'text-teal-500',
-    icon: IconChartCircles,
-    homeLink: '/procurement-analysis',
-    dashboardLink: '/dashboard/procurement-analysis',
-    status: 'coming-soon'
-  },
-  {
-    name: 'Sales report',
-    slug: 'sales-report',
-    description: `Get sales reports in a few steps`,
-    backgroundColor: 'bg-fuchsia-500',
-    textColor: 'text-fuchsia-500',
-    icon: IconChartArrows,
-    homeLink: '/sales-report',
-    dashboardLink: '/dashboard/sales-report',
-    status: 'coming-soon'
-  },
-  {
-    name: 'Financial model',
-    slug: 'financial-model',
-    description: `Analyze your financial model to make better decisions`,
-    backgroundColor: 'bg-blue-500',
-    textColor: 'text-blue-500',
-    icon: IconReportMoney,
-    homeLink: '/financial-model',
-    dashboardLink: '/dashboard/financial-model',
-    status: 'coming-soon'
-  },
+    status: 'coming-soon',
+    categories: ['advisory']
+  }
+];
+
+let categories = [
+  { id: 'featured', name: "Featured" },
+  { id: 'general', name: "General" },
+  { id: 'financial', name: "Financial services" },
+  { id: 'investment', name: "Investment advisory" },
+  { id: 'insurance', name: "Insurance & HMO" },
+  { id: 'cooking', name: "Currently cooking" },
+  { id: 'advisory', name: "Advisory" }
 ];
 
 const DashboardPage = () => {
@@ -157,47 +282,54 @@ const DashboardPage = () => {
           </p>
         </div>
       </div>
-      <div className="container py-10 !max-w-5xl min-h-screen flex flex-col">
-        <div className="grid md:grid-cols-3 gap-4 md:gap-6">
-          {
-            products.map(product => (
-              <Card
-                key={ product.name }
-                onClick={ () => router.push(product.dashboardLink) }
-                className={ classNames(
-                  'rounded-2xl px-10 py-8 transition-all h-full',
-                  { 'disabled': product.status === 'coming-soon' },
-                  { 'hover:-translate-y-1 hover:shadow-md cursor-pointer': product.status === 'active' }
-                ) }
-              >
-                <div>
-                  <div
-                    className={ classNames('rounded-2xl justify-center', product.textColor) }
-                  >
-                    { createElement(product.icon, { size: 44 }) }
-                  </div>
-                </div>
-                <div className="mt-4">
-                  <div className="flex items-center">
-                    <h4 className="font-medium text-[1.06rem]">{ product.name }</h4>
-                    {
-                      product.status === 'coming-soon' && (
+      <div className="container py-10 !max-w-5xl min-h-screen flex flex-col space-y-10">
+        {
+          categories.map(c => (
+            <div key={ c.id }>
+              <h3 className="text-xl font-medium mb-8 px-1">{ c.name }</h3>
+              <div className="grid md:grid-cols-3 gap-4 md:gap-6">
+                {
+                  items.filter(i => i.categories.includes(c.id)).map(item => (
+                    <Card
+                      key={ item.name }
+                      onClick={ () => router.push(item.dashboardLink) }
+                      className={ classNames(
+                        'rounded-2xl px-10 py-8 transition-all h-full',
+                        { 'disabled': item.status === 'coming-soon' },
+                        { 'hover:-translate-y-1 hover:shadow-md cursor-pointer': item.status === 'active' }
+                      ) }
+                    >
+                      <div>
                         <div
-                          className="px-2 py-1 ml-2 leading-none bg-slate-200 text-black rounded-full text-xs inline-flex"
+                          className={ classNames('rounded-2xl justify-center', item.textColor) }
                         >
-                          Alpha
+                          { createElement(item.icon, { size: 44 }) }
                         </div>
-                      )
-                    }
-                  </div>
-                  <p className="leading-tight mt-1">
-                    { product.description }
-                  </p>
-                </div>
-              </Card>
-            ))
-          }
-        </div>
+                      </div>
+                      <div className="mt-4">
+                        <div className="flex items-center">
+                          <h4 className="font-medium text-[1.06rem]">{ item.name }</h4>
+                          {
+                            item.status === 'coming-soon' && (
+                              <div
+                                className="px-2 py-1 ml-2 leading-none bg-slate-200 text-black rounded-full text-xs inline-flex"
+                              >
+                                Alpha
+                              </div>
+                            )
+                          }
+                        </div>
+                        <p className="leading-tight mt-1">
+                          { item.description }
+                        </p>
+                      </div>
+                    </Card>
+                  ))
+                }
+              </div>
+            </div>
+          ))
+        }
       </div>
     </>
   );
