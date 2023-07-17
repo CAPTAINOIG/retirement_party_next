1cb4dbf3d0270ebacc8cd4834fde29c83447b96d
Jude <judecodes@gmail.com>
2023-07-17 21:56:36 +0100

    feat: add hover effect to Card component
    The Card now supports a hover state and changes background color upon hover for better visual feedback. This change adds hover as an optional prop to the Card component, defaulting to false. When hover is true, additional CSS classes are applied to change the background color and cursor

--- src/components/global/Card.jsx
+++ src/components/global/Card.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import classNames from "classnames";
 
-const Card = ({ className, children, ...props }) => {
+const Card = ({ className, children, hover = false, ...props }) => {
   return (
-    <div className={ classNames('bg-white shadow border rounded-[1.2rem]', className) } { ...props }>
+    <div
+      className={ classNames(
+        'bg-white shadow border rounded-[1.2rem] transition-all duration-300', className,
+        { 'hover:bg-slate-100 cursor-pointer': hover }
+      ) } { ...props }
+    >
       { children }
     </div>
   );
