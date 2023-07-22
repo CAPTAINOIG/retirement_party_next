ee1c2d173dfcca997f24f16df9c71402336b0eff
Jude <judecodes@gmail.com>
2023-07-22 14:01:56 +0100

    feat(dashboard): product appearance and add featured card

--- src/app/dashboard/(root)/page.jsx
+++ src/app/dashboard/(root)/page.jsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/navigation";
 import Card from "@/components/global/Card";
 import products, { categories } from "@/lib/products";
 
+const featured = 'custom-report';
+
 const DashboardPage = () => {
   const router = useRouter();
 
@@ -24,47 +26,43 @@ const DashboardPage = () => {
         </div>
       </div>
       <div className="container py-12 md:py-16 !max-w-5xl min-h-screen flex flex-col space-y-10">
+        <div>
+          <h3 className="text-xl font-medium mb-8 px-1">Featured</h3>
+          <div className="grid md:grid-cols-2 gap-4 md:gap-6">
+            {
+              products.filter(p => p.slug === featured).map(product => (
+                <FeaturedProductCard
+                  product={ product }
+                  key={ product.name }
+                  onClick={ () => router.push(product.dashboardLink) }
+                />
+              ))
+            }
+            <div className="grid gap-6">
+              {
+                products.filter(p => p.categories.includes('featured') && (p.slug !== featured)).map(product => (
+                  <ProductCard
+                    product={ product }
+                    key={ product.name }
+                    onClick={ () => router.push(product.dashboardLink) }
+                  />
+                ))
+              }
+            </div>
+          </div>
+        </div>
         {
-          categories.map(c => (
+          categories.filter(c => c.id !== 'featured').map(c => (
             <div key={ c.id }>
               <h3 className="text-xl font-medium mb-8 px-1">{ c.name }</h3>
               <div className="grid md:grid-cols-3 gap-4 md:gap-6">
                 {
                   products.filter(p => p.categories.includes(c.id)).map(product => (
-                    <Card
+                    <ProductCard
+                      product={ product }
                       key={ product.name }
                       onClick={ () => router.push(product.dashboardLink) }
-                      className={ classNames(
-                        'rounded-2xl px-10 py-8 transition-all h-full',
-                        { 'disabled': product.status === 'coming-soon' },
-                        { 'hover:-translate-y-1 hover:shadow-md cursor-pointer': product.status === 'active' }
-                      ) }
-                    >
-                      <div>
-                        <div
-                          className={ classNames('rounded-2xl justify-center', product.textColor) }
-                        >
-                          { createElement(product.icon, { size: 44 }) }
-                        </div>
-                      </div>
-                      <div className="mt-4">
-                        <div className="flex items-center">
-                          <h4 className="font-medium text-[1.06rem]">{ product.name }</h4>
-                          {
-                            product.status === 'coming-soon' && (
-                              <div
-                                className="px-2 py-1 ml-2 leading-none bg-slate-200 text-black rounded-full text-xs inline-flex"
-                              >
-                                Alpha
-                              </div>
-                            )
-                          }
-                        </div>
-                        <p className="leading-tight mt-1">
-                          { product.description }
-                        </p>
-                      </div>
-                    </Card>
+                    />
                   ))
                 }
               </div>
@@ -77,3 +75,68 @@ const DashboardPage = () => {
 };
 
 export default requireAuth(DashboardPage);
+
+const ProductCard = ({ product, onClick }) => {
+  return (
+    <Card
+      onClick={ onClick }
+      className={ classNames(
+        'rounded-2xl px-10 py-8 transition-all h-full relative overflow-hidden',
+        { 'disabled': product.status === 'coming-soon' },
+        { 'hover:-translate-y-1 hover:shadow-md cursor-pointer': product.status === 'active' }
+      ) }
+    >
+      <div>
+        <div
+          className={ classNames('rounded-2xl justify-center', product.textColor) }
+        >
+          { createElement(product.icon, { size: 44 }) }
+        </div>
+      </div>
+      <div className="mt-4">
+        <div className="flex items-center">
+          <h4 className="font-medium text-[1.06rem]">{ product.name }</h4>
+          {
+            product.status === 'coming-soon' && (
+              <div
+                className="absolute top-0 right-0 px-3 py-1 ml-2 leading-none bg-slate-100 text-black rounded-bl-lg text-xs inline-flex"
+              >
+                Coming soon
+              </div>
+            )
+          }
+        </div>
+        <p className="leading-tight opacity-80 mt-1">
+          { product.description }
+        </p>
+      </div>
+    </Card>
+  );
+};
+
+
+const FeaturedProductCard = ({ product, onClick }) => {
+  return (
+    <Card
+      onClick={ onClick }
+      className={ classNames(
+        'rounded-2xl px-10 py-8 transition-all h-full border-0 !shadow-none text-white hover:-translate-y-1 cursor-pointer flex flex-col justify-center',
+        product.backgroundColor,
+      ) }
+    >
+      <div>
+        <div className="rounded-2xl justify-center text-white">
+          { createElement(product.icon, { size: 100 }) }
+        </div>
+      </div>
+      <div className="mt-12">
+        <div className="flex items-center">
+          <h4 className="font-medium text-2xl">{ product.name }</h4>
+        </div>
+        <p className="text-[1.1rem] leading-snug mt-2 opacity-80 max-w-[200px]">
+          { product.description }
+        </p>
+      </div>
+    </Card>
+  );
+};
