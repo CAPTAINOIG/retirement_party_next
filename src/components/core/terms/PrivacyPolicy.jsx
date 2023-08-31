1b8758616a8726baf138be6808984ef51ebd7633
Jude <judecodes@gmail.com>
2023-08-31 13:11:59 +0100

    feat: add 'Terms and Conditions' and 'Privacy Policy' pages
    New 'Terms and Conditions' and 'Privacy Policy' components were created and added to the website. These pages clearly outline the website's privacy policy and terms of service for the users. This update ensures that the website is complying with legal requirements of disclosure and provides transparency for users about how their information is managed.

---
+++ src/components/core/terms/PrivacyPolicy.jsx
@@ -0,0 +1,163 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="space-y-6">
+      <p className="mb-4">
+        Statisense doing business as Statisense operates the website https://www.statisense.co (from now on the
+        website).
+        <br/>
+        This privacy policy is intended to describe how Statisense collects, uses, and shares personal information from
+        consumer users of this website. This policy establishes how we will treat your personal information, so it is
+        necessary for you to familiarize yourself with it.
+      </p>
+      <ol className="list-decimal list-inside space-y-6">
+        <li>
+          <h3 className="inline-flex font-semibold mb-4">TYPE OF INFORMATION THAT WE STORE</h3>
+          <p className="mb-4">
+            Statisense guarantees the total protection of the personal information of its visitors as well as that of
+            its clients. We do not collect personal information from website visitors except in cases where information
+            is provided by choice and also the cases that may be listed below in this document.
+          </p>
+          <p className="mb-4">
+            Statisense does not sell, shares or discloses personal information of visitors or customers with third
+            parties or other organizations.
+          </p>
+          <p className="mb-4">
+            We may collect, store and use the following information:
+          </p>
+          <div className="mb-4 ml-6">
+            <p className="mb-4">
+              Information you give us: We are likely to collect your email, address, telephone number, as well as other
+              information that you have given us directly via our website for the purpose of registering or subscribing
+              to any of our services, events, notifications , etc.
+              <br/>
+              Information we obtain from third parties: We are likely to obtain information about you from other sources
+              (such as LinkedIn or Facebook), information that can be added to our own base.
+              <br/>
+              Automatically collected information: Refers to any type of information that we obtain directly from your
+              visits or use of this site.
+            </p>
+          </div>
+        </li>
+        <li>
+          <h3 className="inline-flex font-semibold mb-4">USE OF COOKIES</h3>
+          <p className="mb-4">
+            Statisense (statisense.co) uses cookies (data files stored on your hard drive by a website) each time you
+            access our online platform. We use two types of cookies:
+          </p>
+          <div className="mb-4 ml-6">
+            <p className="mb-4">
+              Session Cookies: Which disappear automatically once you close your browser.<br/>
+              Persistent cookies: which remain on your computer even after closing the browser, and are used during your
+              next visits to our website.
+            </p>
+            <p className="mb-4">
+              The use of Cookies by Statisense has as its goal to offer a much more personal and interactive experience
+              to the user. However, you can block the use of Cookies by directly accessing the settings or configuration
+              of your computer and managing the use of cookies.
+            </p>
+            <p className="mb-4">
+              Please note that this may affect and/or limit the use of certain services on our website.
+            </p>
+          </div>
+        </li>
+        <li>
+          <h3 className="inline-flex font-semibold mb-4">WITH WHOM WE SHARE THE INFORMATION OBTAINED</h3>
+          <p className="mb-4">
+            Statisense does NOT obtain, collect, or administer any personal information related to your race, ethnicity,
+            religion, philosophical practices, beliefs, personal or sexual life, political opinions, criminal records,
+            special memberships, health, genetics or biometric data.
+          </p>
+          <p className="mb-4">
+            That being said, the information that we do use, can be potentially shared with the following people, for
+            the following reasons:
+          </p>
+          <p className="mb-4 ml-6">
+            With members of our team: We may share your information with any member of our team, including affiliated
+            members, subsidiaries or staff of our branches or those who work directly with us (Statisense).
+            <br/>
+            With service providers: Because we work with service providers to provide certain development,
+            communication, hosting and maintenance functions, it is possible that these third parties have access to the
+            information collected. However, we require these suppliers to only process or manipulate the personal
+            information obtained according to our own rules and instructions, always within the legal framework.
+            <br/>
+            To third parties (as required by law): It is possible that, according to the requirements of the law, we are
+            obliged to share the information obtained with third parties.
+          </p>
+        </li>
+        <li>
+          <h3 className="inline-flex font-semibold mb-4">WHY WE STORE YOUR INFORMATION</h3>
+          <p className="mb-4">
+            Among the reasons we store your information, we collect and store it for:
+          </p>
+          <ul className="list-disc list-inside mb-4 ml-6">
+            <li>To be able to share information for legal, security or protection purposes in accordance with the law.
+            </li>
+            <li>To be able to share information in cases of emergency.</li>
+            <li>To be able to share information with any future or potential buyer or seller that is part of the
+              business in which we are involved.
+            </li>
+          </ul>
+          <p className="mb-4">Similarly, we may use the information obtained to:</p>
+          <ul className="list-disc list-inside mb-4 ml-6">
+            <li>Understand, study and analyze the preferences, trends and behavior of our users.</li>
+            <li>Understand, study and analyze the way in which users interact with our platform and improve the quality
+              of the services offered.
+            </li>
+            <li>Provide better support to current users of Statisense if necessary.</li>
+            <li>Manage and create promotions according to the relationship of each user with the services and products
+              offered by Statisense.
+            </li>
+            <li>Administrative or marketing purposes, which may include but not be limited to improvements in the
+              communication service, promotions update, product improvements, etc.
+            </li>
+            <li>Enforce our terms of service.</li>
+            <li>Comply with any legal requirement to which we are obligated.</li>
+          </ul>
+        </li>
+        <li>
+          <h3 className="inline-flex font-semibold mb-4">DATA SECURITY:</h3>
+          <p className="mb-4">
+            Your data is safe with us. Statisense takes all necessary precautions to avoid any type of loss,
+            destruction, alteration or misuse of your data and personal information. However, keep in mind that this
+            can’t guarantees that your information can not be attacked, especially considering the nature of the
+            internet.
+          </p>
+        </li>
+        <li>
+          <h3 className="inline-flex font-semibold mb-4">THIRD PARTY WEBSITES</h3>
+          <p className="mb-4">
+            Our website (statisense.co) has links or links that lead directly to other websites. We are NOT responsible
+            for what happens with your information on those websites, nor are we responsible for the privacy policies of
+            sites external to Statisense.
+          </p>
+        </li>
+      </ol>
+      <div>
+        <h3 className="inline-flex font-semibold mb-4">CHANGES AND IMPROVEMENTS IN OUR PRIVACY POLICY</h3>
+        <p className="mb-4">
+          The terms, clauses and information presented in this privacy policy document may face changes as time
+          progresses and we change some of our conditions.
+          Statisense reserves the right to do so, and emphasizes the user's responsibility to periodically verify the
+          website to obtain updates regarding our current policies. It is also reminded that any prolongation of the use
+          of the website after any change made constitutes the immediate acceptance of these changes.
+        </p>
+      </div>
+      <div>
+        <h3 className="inline-flex font-semibold mb-4">CONTACT INFORMATION</h3>
+        <p className="mb-4">
+          For comments and questions about our privacy policy, contact us through the following address and phone
+          number:
+          <br/>
+          Lagos:
+          <br/>
+          Address: 1038B, Ologun Agbaje Street, Off Adeola Odeku,Victoria Island, Lagos, Nigeria.
+          <br/><a href="tel:+2349018001800" className="opacity-70 inline-flex mt-2">+2349018001800</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
---
+++ src/components/core/terms/TermsAndConditions.jsx
@@ -0,0 +1,284 @@
+import React from 'react';
+
+const TermsAndConditions = () => {
+  return (
+    <ol className="list-decimal list-inside space-y-6">
+      <li>
+        <h3 className="inline-flex font-semibold mb-4">Overview</h3>
+        <p className="mb-4">
+          The following are the terms of an agreement between you and Statisense. By accessing, or using
+          this Website, you acknowledge that you have read, understand, and agree to be bound by these terms
+          and
+          to comply with all applicable laws and regulations, including export and re-export control laws and
+          regulations. If you do not agree to these terms, please do not use this Website.
+          Statisense may, without notice to you, at any time, revise these Terms of Use and any other
+          information contained in this Website. Statisense may also make improvements or changes in the
+          products,
+          services, or programs described in this site at any time without notice.
+        </p>
+      </li>
+      <li>
+        <h3 className="inline-flex font-semibold mb-4">General</h3>
+        <p className="mb-4">
+          This Website contains proprietary notices and copyright information, the terms of which must be
+          observed
+          and followed. Please see the tab entitled “Copyright and trademark information” for related
+          information.
+        </p>
+        <p className="mb-4">
+          Statisense grants you a non-exclusive, non-transferable, limited permission to access and display
+          the Web
+          pages within this site as a customer or potential customer of Statisense provided you comply with
+          these
+          Terms of Use, and all copyright, trademark, and other proprietary notices remain intact. You may
+          only use
+          a crawler to crawl this Website as permitted by this Website’s robots.txt protocol, and Statisense
+          may
+          block any crawlers in its sole discretion. The use authorized under this agreement is non-commercial
+          in
+          nature (e.g., you may not sell the content you access on or through this Website.) All other use of
+          this
+          site is prohibited.
+        </p>
+        <p className="mb-4">
+          Except for the limited permission in the preceding paragraph,Statisense does not grant you any
+          express or
+          implied rights or licenses under any patents, trademarks, copyrights, or other proprietary or
+          intellectual
+          property rights. You may not mirror any of the content from this site on another Website or in any
+          other
+          media. Any software and other materials that are made available for downloading, access, or other
+          use from
+          this site with their own license terms will be governed by such terms, conditions, and notices. Your
+          failure to comply with such terms or any of the terms on this site will result in automatic
+          termination of
+          any rights granted to you, without prior notice, and you must immediately destroy all copies of
+          downloaded
+          materials in your possession, custody or control.
+        </p>
+      </li>
+      <li>
+        <h3 className="inline-flex font-semibold mb-4">Rules of conduct</h3>
+        <p className="mb-4">
+          Your use of Statisense website link and any related Statisense websites (Statisense Websites) is
+          subject
+          to all applicable local, state, national and international laws and regulations, and you agree not
+          to
+          violate such laws and regulations. Any attempt by any person to deliberately damage Statisense
+          Websites is
+          a violation of criminal and civil laws. Statisense reserves the right to seek damages from any such
+          person
+          to the fullest extent permitted by law.
+        </p>
+        <p className="mb-4">
+          You agree not to post or transmit through Statisense Websites any material or content that violates
+          or
+          infringes in any way the rights of others or solicits, encourages or promotes the use of illegal
+          substances or activities, which is unlawful, threatening, abusive, harassing, defamatory, libelous,
+          derogatory, invasive of privacy or publicity rights, vulgar, obscene, bigoted or hateful, profane,
+          scandalous, pornographic, indecent or otherwise objectionable, gives rise to civil or criminal
+          liability
+          or otherwise violates any applicable law.
+        </p>
+        <p className="mb-4">
+          You may not impersonate another user, use another user’s id, permit someone else to use your id,
+          attempt
+          to capture or guess other users’ passwords, forge headers or otherwise manipulate identifiers for
+          the
+          purpose of disguising the origin of any content you transmit, conduct fraudulent business operations
+          or
+          practices, or promote or conceal unlawful conduct.
+        </p>
+        <p className="mb-4">
+          You may not engage in any activity on Statisense Websites which restrict or inhibit any other user
+          from
+          using or enjoying StatisenseWebsites by “hacking,” “cracking,” “spoofing,” or defacing any portions
+          of
+          Statisense Websites. You may not use Statisense Websites to stalk or harass another person or
+          entity.
+        </p>
+        <p className="mb-4">
+          You may not post or transmit through Statisense Websites advertising or commercial solicitations;
+          promotional materials relating to website or online services which are competitive with Statisense
+          and/or
+          Statisense Websites; software or other materials that contain viruses, worms, time bombs, Trojan
+          horses,
+          or other harmful or disruptive components, political campaign materials; chain letters; mass
+          mailings,
+          spam mail, any robot, spider, site search/retrieval application, or other manual or automatic device
+          or
+          process to retrieve, index, “data mine,” or in any way reproduce or circumvent the navigational
+          structure
+          or presentation of Statisense Websites or contents. You may not harvest or collect information about
+          website visitors without their express written consent.
+        </p>
+      </li>
+      <li>
+        <h3 className="inline-flex font-semibold mb-4">Your content</h3>
+        <p className="mb-4">
+          You agree that you will only upload, share, post, publish, transmit, or otherwise make available
+          ("Share")
+          on or through the Site Content that you have the right and authority to Share and for which you have
+          the
+          right and authority to grant to all of the licenses and rights set forth herein. By Sharing Content,
+          you
+          grant Statisense a worldwide, perpetual, royalty-free, irrevocable, nonexclusive, fully
+          sublicensable
+          license to use, reproduce, modify, adapt, translate, publish, publicly perform, publicly display,
+          broadcast, transmit and distribute the Content for any purpose and in any form, medium, or
+          technology now
+          known or later developed. This includes, without limitation, the right to incorporate or implement
+          the
+          Content into any Statisense product or service, and to display, market, sublicense and distribute
+          the
+          Content as incorporated or embedded in any product or service distributed or offered by Statisense
+          without
+          compensation to you. You warrant that: (a) you have the right and authority to grant this license;
+          (b)
+          Statisense's exercise of the rights granted pursuant to this license will not infringe or otherwise
+          violate any third party rights; and (c) all so-called moral rights in the Content have been waived
+          to the
+          full extent allowed by law.
+        </p>
+        <p className="mb-4">
+          You agree that you will neither use the Site in a manner, nor Share any Content, that: (a) is false
+          or
+          misleading; (b) is defamatory, derogatory, degrading or harassing of another or constitutes a
+          personal
+          attack; (c) invades another's privacy or includes, copies or transmits another's confidential,
+          sensitive
+          or personal information; (d) promotes bigotry, racism, hatred or harm against any group or
+          individual; (e)
+          is obscene or not in good taste; (f) violates or infringes or promotes the violation or infringement
+          of
+          another's rights, including intellectual property rights; (g) you do not have the right and
+          authority to
+          Share and grant the necessary rights and licenses for; (h) violates or promotes the violation of any
+          applicable laws or regulations; (i) contains a solicitation of funds, goods or services, or promotes
+          or
+          advertises goods or services; or (j) contains any viruses, Trojan horses, or other components
+          designed to
+          limit or harm the functionality of a computer. Statisense may report you to the relevant authorities
+          and
+          may act under the fullest extent of applicable laws if you transmit or upload content intended or
+          designed
+          to cause harm.
+        </p>
+      </li>
+      <li>
+        <h3 className="inline-flex font-semibold mb-4">Disclaimer</h3>
+        <p className="mb-4">
+          From time to time, this Website may contain technical inaccuracies or typographical errors, and we
+          do not
+          warrant the accuracy of any posted information. Please confirm you are using the most up-to-date
+          pages on
+          this Website, and confirm the accuracy and completeness of information before using it to make
+          decisions
+          relating to services, products, or other matters described in this Website. If any term in this
+          Terms of
+          Use is found by competent judicial authority to be unenforceable in any respect, the validity of the
+          remainder of this Terms of Use will be unaffected, provided that such unenforceability does not
+          materially
+          affect the parties’ rights under this Terms of Use. Forward-looking and cautionary statements Except
+          for
+          historical information and discussions, statements set forth throughout this web site may constitute
+          forward-looking statements within the meaning of the Private Securities Litigation Reform Act of
+          1995 or
+          other applicable laws. These statements involve a number of risks, uncertainties, and other factors
+          that
+          could cause actual results to differ materially, as discussed in the company's filings with the U.S.
+          Securities and Exchange Commission. See the “SEC filings” tab under “Investor relations” on this Web
+          site
+          for copies of such filings.
+        </p>
+      </li>
+      <li>
+        <h3 className="inline-flex font-semibold mb-4">Confidential information</h3>
+        <p className="mb-4">
+          Statisense does not want to receive confidential or proprietary information from you through our Web
+          site.
+          Please note that any information or material sent to will be deemed NOT to be confidential. By
+          sending
+          Statisense any information or material, you grant Statisense an unrestricted, irrevocable license to
+          copy,
+          reproduce, publish, upload, post, transmit, distribute, publicly display, perform, modify, create
+          derivative works from, and otherwise freely use, those materials or information. You also agree that
+          Statisense is free to use any ideas, concepts, know-how, or techniques that you send us for any
+          purpose.
+          However, we will not release your name or otherwise publicize the fact that you submitted materials
+          or
+          other information to us unless: (a) we obtain your permission to use your name; or (b) we first
+          notify you
+          that the materials or other information you submit to a particular part of this site will be
+          published or
+          otherwise used with your name on it; or (c) we are required to do so by law. Personally-identifiable
+          information that you submit to Statisense for the purpose of receiving products or services will be
+          handled in accordance with our privacy policies. Please see the tab entitled “Privacy” for
+          information
+          regarding Statisense’s privacy policies.
+        </p>
+      </li>
+      <li>
+        <h3 className="inline-flex font-semibold mb-4">Linking to this site</h3>
+        <p>
+          Statisense consents only to links to this Website in which the link and the pages that are
+          activated by
+          the link do not: (a) create frames around any page on this Website or use other techniques that
+          alter in
+          any way the visual presentation or appearance of any content within this site; (b) misrepresent your
+          relationship with Statisense; (c) imply that Statisense approves or endorses you, your Website, or
+          your
+          service or product offerings; and (d) present false or misleading impressions about Statisense or
+          otherwise damage the goodwill associated with the Statisense name or trademarks. As a further
+          condition to
+          being permitted to link to this site, you agree that Statisense may at any time, in its sole
+          discretion,
+          terminate permission to link to this Website. In such event, you agree to immediately remove all
+          links to
+          this Website and to cease any related use of Statisense trademarks.
+        </p>
+      </li>
+      <li>
+        <h3 className="inline-flex font-semibold mb-4">Disclaimer of warranty</h3>
+        <p className="mb-4">
+          Use of this site is at your sole risk. All materials, information, products, software, programs, and
+          services are provided "as is," with no warranties or guarantees whatsoever. Statisense expressly
+          disclaims
+          to the fullest extent permitted by law all express, implied, statutory, and other warranties,
+          guarantees,
+          or representations, including, without limitation, the warranties of merchantability, fitness for a
+          particular purpose, and non-infringement of proprietary and intellectual property rights. Without
+          limitation, Statisense makes no warranty or guarantee that this web site will be uninterrupted,
+          timely,
+          secure, or error-free. You understand and agree that if you download or otherwise obtain materials,
+          information, products, software, programs, or services from this web site, you do so at your own
+          discretion and risk and that you will be solely responsible for any damages that may result,
+          including
+          loss of data or damage to your computer system. Some jurisdictions do not allow the exclusion of
+          warranties, so the above exclusions may not apply to you.
+        </p>
+      </li>
+      <li>
+        <h3 className="inline-flex font-semibold mb-4">Limitation of liability</h3>
+        <p>
+          To the fullest extent permitted by applicable law, in no event will Statisense be liable to any
+          party for
+          any direct, indirect, incidental, special, exemplary or consequential damages of any type whatsoever
+          related to or arising from this web site or any use of this web site, or of any site or resource
+          linked
+          to, referenced, or accessed through this web site, or for the use or downloading of, or access to,
+          any
+          materials, information, products, or services, including, without limitation, any lost profits,
+          business
+          interruption, lost savings or loss of programs or other data, even if Statisense is expressly
+          advised of
+          the possibility of such damages. This exclusion and waiver of liability applies to all causes of
+          action,
+          whether based on contract, warranty, tort, or any other legal theories.
+        </p>
+      </li>
+    </ol>
+  );
+};
+
+export default TermsAndConditions;
