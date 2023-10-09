85c6e83a0adef04641feb04153d8bd467a31a2c2
Jude <judecodes@gmail.com>
2023-10-09 05:42:13 +0100

    feat(contact): temporarily remove whatsapp

--- src/components/core/contact/ConnectWithUs.jsx
+++ src/components/core/contact/ConnectWithUs.jsx
@@ -3,7 +3,6 @@ import {
   IconBrandInstagram,
   IconBrandLinkedin,
   IconBrandThreads,
-  IconBrandWhatsapp,
   IconBrandX,
   IconMail
 } from "@tabler/icons-react";
@@ -27,17 +26,6 @@ const ConnectWithUs = () => {
             <p>Email address</p>
             <a href="mailto:info@statisense.com.ng" target="_blank" className="flex font-medium">
               info@statisense.com.ng
-            </a>
-          </div>
-        </div>
-        <div className="flex items-center space-x-3">
-          <div className="w-10 h-10 rounded-full flex items-center justify-center border">
-            <IconBrandWhatsapp size="20"/>
-          </div>
-          <div>
-            <p>Whatsapp</p>
-            <a href="https://wa.me/xxxxx" target="_blank" className="flex font-medium">
-              +2340000000000
             </a>
           </div>
         </div>
