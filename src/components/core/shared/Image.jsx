f9fa0044c568e48967ae4d007c46a9f5f76abd99
Jude <judecodes@gmail.com>
2023-10-19 17:34:29 +0100

    chore: update S3 bucket usage in Image component
    The S3 bucket name is now being retrieved from environment variables rather than being hardcoded. This provides flexibility in choosing different buckets without changing the code and enhances security as bucket names are not exposed directly in the code anymore.

--- src/components/core/shared/Image.jsx
+++ src/components/core/shared/Image.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+const bucket = process.env.NEXT_PUBLIC_S3_BUCKET_NAME;
+
 const Image = ({ src, alt, ...props }) => {
   return (
-    <img src={ `https://statisense.s3.amazonaws.com/${ src }` } alt={ alt } { ...props }/>
+    <img src={ `https://${ bucket }.s3.amazonaws.com/${ src }` } alt={ alt } { ...props }/>
   );
 };
 
