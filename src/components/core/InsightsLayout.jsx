de7e39e1655df6d31e4ff54edaeee43f2c0d5396
Jude <judecodes@gmail.com>
2023-11-21 23:41:28 +0100

    update insights layout

--- src/components/core/InsightsLayout.jsx
+++ src/components/core/InsightsLayout.jsx
@@ -6,17 +6,19 @@ import AskBambiCard from '@/components/core/infographics/AskBambiCard';
 const InsightsLayout = ({ children }) => {
   return (
     <div className="bg-slate-100 py-12 md:py-16">
-      <div className="container !max-w-5xl">
-        <div className="grid md:grid-cols-[310px_1fr] gap-12 relative">
+      <div className="container">
+        <div className="grid lg:grid-cols-[280px_1fr_310px] gap-8 relative">
           <div>
             <div className="sticky top-32 grid gap-8">
-              <div className="order-1 md:order-none">
-                <InsightsSideNav />
-              </div>
+              <InsightsSideNav />
+            </div>
+          </div>
+          <div>{children}</div>
+          <div className="hidden lg:block">
+            <div className="sticky top-32 grid gap-8">
               <AskBambiCard />
             </div>
           </div>
-          <div className="order-1 md:order-none">{children}</div>
         </div>
       </div>
     </div>
