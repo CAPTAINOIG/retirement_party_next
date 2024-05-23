684cb1924259fe008e93104867fb4c4049eb5542
Jude <judecodes@gmail.com>
2024-05-23 13:57:59 +0100

    feat: replace typedjs with word rotate

--- src/components/core/home/Hero.jsx
+++ src/components/core/home/Hero.jsx
@@ -1,30 +1,12 @@
-import React, { useEffect, useRef } from 'react';
-import Typed from 'typed.js';
+import React from 'react';
 import Button from '@/components/global/Button';
 import { IconChevronRight } from '@tabler/icons-react';
 import Link from 'next/link';
+import WordRotate from '@/components/global/WordRotate';
 
 const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 
 const Hero = () => {
-  const el = useRef(null);
-  const typed = useRef(null);
-
-  useEffect(() => {
-    const options = {
-      strings: ['Generative AI', 'Banking AI', 'Identity AI', 'Markets AI'],
-      typeSpeed: 50,
-      backSpeed: 10,
-      backDelay: 3000,
-      loop: true,
-      smartBackspace: true,
-    };
-    typed.current = new Typed(el.current, options);
-    return () => {
-      typed.current.destroy();
-    };
-  }, []);
-
   return (
     <div className="to-primary-950 relative overflow-hidden bg-[#11100f] bg-gradient-to-br from-primary-900 pb-32 pt-48 md:pb-32 md:pt-52 md:text-center">
       <div className="absolute inset-0 min-h-screen animate-wide bg-hero bg-cover bg-right opacity-60"></div>
@@ -32,13 +14,7 @@ const Hero = () => {
         <h1 className="font-display mx-auto max-w-[860px] text-[3.4rem] font-bold leading-[1.1] tracking-tight text-slate-300 sm:text-7xl md:text-[6.8rem] md:!leading-[1]">
           Connecting
           <br />
-          <span
-            ref={el}
-            className="relative whitespace-nowrap bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent"
-          >
-            Generative AI
-          </span>
-          <br />
+          <WordRotate words={['Generative AI', 'Banking AI', 'Identity AI', 'Markets AI']} duration={5000} />
           to Africa's Data
         </h1>
         <p className="mx-auto mt-8 max-w-xl text-lg tracking-tight text-slate-400">
