08e2e5edab73e32c0d9da5cd9f51d093fca711a1
Jude <judecodes@gmail.com>
2025-01-29 10:20:07 +0100

    feat(middleware): add redirect for /investordeck to external link

---
+++ src/middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  if (request.nextUrl.pathname === '/investordeck') {
+    return NextResponse.redirect(
+      new URL('https://drive.google.com/file/d/1iUrEK2Maxo--GGeYJ7rBzPSj-S6ycfJF/view?usp=sharing')
+    );
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/investordeck',
+};
