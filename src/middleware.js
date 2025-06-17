cdbc28b62d6ce6c288865b367445f91c620e5e2c
Jude Francis <judecodes@gmail.com>
2025-06-17 14:54:00 +0100

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -11,9 +11,13 @@ export function middleware(request) {
       new URL('https://drive.google.com/file/d/1vo9mlA-Bo1_vWXqj58VQBqy-mYr77bxI/view?usp=sharing')
     );
   }
+if (request.nextUrl.pathname === '/productinvestor') {
+return new URL('https://drive.google.com/file/d/18AIx44g9hG1LssX0QldppegIAWxtxGcQ/view?usp=drivesdk');
+}
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/investordeck', '/2pagerdeck'],
+  matcher: ['/investordeck', '/2pagerdeck', '/productinvestor'],
 };
