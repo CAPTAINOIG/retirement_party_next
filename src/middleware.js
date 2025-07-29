132ad229b367fd3e91dd25e2254118f1a063f4e0
Jude <judecodes@gmail.com>
2025-07-29 08:45:40 +0100

    feat: add redirect for /dataroom path in middleware
    - Implemented a new redirect for the '/dataroom' path to a Dropbox link for enhanced resource access.
    - Updated the middleware matcher to include the new '/dataroom' route for proper handling.

--- src/middleware.js
+++ src/middleware.js
@@ -26,10 +26,17 @@ export function middleware(request) {
       new URL('https://drive.google.com/file/d/1iRMp0cueG99ehRp7-RX3SbMce9y_dizw/view?usp=sharing')
     );
   }
+  if (request.nextUrl.pathname.toLowerCase() === '/dataroom') {
+    return NextResponse.redirect(
+      new URL(
+        'https://www.dropbox.com/scl/fo/egibgzo6b1gs0h9bxvo87/ACfXbWT6hECJjGIKE2hCN84?rlkey=ltqyl0p24lfd471t40zbu3oju&st=47ag9hub&dl=0'
+      )
+    );
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/investordeck', '/2pagerdeck', '/ideck', '/ideck_teaser', '/product(.*)'],
+  matcher: ['/investordeck', '/2pagerdeck', '/ideck', '/ideck_teaser', '/product(.*)', '/dataroom'],
 };
