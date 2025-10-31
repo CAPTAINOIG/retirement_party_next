6a59431cafe954530c24e75afb0133256ee9fea1
Jude <judecodes@gmail.com>
2025-10-31 09:46:43 +0100

    feat(analysts/tracking): Add Twitter pixel tracking for Analysts page
    - Integrate Twitter pixel script for page view tracking
    - Add useEffect hook to trigger page view tracking
    - Implement script loading with Next.js Script component
    - Configure Twitter pixel with 'qa5ef' tracking code
    - Ensure tracking only occurs on client-side

--- src/app/analysts/content.jsx
+++ src/app/analysts/content.jsx
@@ -1,6 +1,7 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import Script from 'next/script';
 import HeroSection from './components/HeroSection';
 import IntroSection from './components/IntroSection';
 import WhyJoinSection from './components/WhyJoinSection';
@@ -9,38 +10,61 @@ import WhoCanJoinSection from './components/WhoCanJoinSection';
 import CallToActionSection from './components/CallToActionSection';
 
 const AnalystsGalleryContent = () => {
+  useEffect(() => {
+    // Track page view for analysts page specifically
+    if (typeof window !== 'undefined' && window.twq) {
+      window.twq('track', 'PageView');
+    }
+  }, []);
+
   return (
-    <div className="dark min-h-screen bg-black text-white">
-      {/* Hero Section */}
-      <section className="relative">
-        <HeroSection />
-      </section>
+    <>
+      {/* Twitter Pixel Script for Analysts Page */}
+      <Script
+        id="twitter-pixel-analysts"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','qa5ef');
+          `,
+        }}
+      />
 
-      {/* Intro Section */}
-      <section className="relative">
-        <IntroSection />
-      </section>
+      <div className="dark min-h-screen bg-black text-white">
+        {/* Hero Section */}
+        <section className="relative">
+          <HeroSection />
+        </section>
 
-      {/* Why Join Section */}
-      <section className="relative">
-        <WhyJoinSection />
-      </section>
+        {/* Intro Section */}
+        <section className="relative">
+          <IntroSection />
+        </section>
 
-      {/* Who Can Join Section */}
-      <section className="relative">
-        <WhoCanJoinSection />
-      </section>
+        {/* Why Join Section */}
+        <section className="relative">
+          <WhyJoinSection />
+        </section>
 
-      {/* How It Works Section */}
-      {/* <section className="relative">
-        <HowItWorksSection />
-      </section> */}
+        {/* Who Can Join Section */}
+        <section className="relative">
+          <WhoCanJoinSection />
+        </section>
 
-      {/* Call to Action Section */}
-      <section className="relative">
-        <CallToActionSection />
-      </section>
-    </div>
+        {/* How It Works Section */}
+        {/* <section className="relative">
+          <HowItWorksSection />
+        </section> */}
+
+        {/* Call to Action Section */}
+        <section className="relative">
+          <CallToActionSection />
+        </section>
+      </div>
+    </>
   );
 };
 
