b36724d77bc85d1b2d0d2993be474ef6d4b9a08f
Abdullahi Samsudeen <abdullahisamsudeen@gmail.com>
2026-05-20 11:40:10 +0100

    chat widget removed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -160,25 +160,6 @@ const RootLayout = ({ children }) => {
             `,
           }}
         />
-
-        {/* Tawk.to Live Chat */}
-        <Script
-          id="tawkto-widget"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/689ff881de044319242d08b4/1j2oe5f5g';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-          }}
-        />
       </body>
     </html>
   );
